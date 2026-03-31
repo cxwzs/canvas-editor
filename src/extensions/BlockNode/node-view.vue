eb1e75a0aa0d1557603cea19f754726c01ab78cf
chxw <786005461@qq.com>
2026-03-31 16:20:08 +0800

    fix:图片拉伸

--- src/extensions/BlockNode/node-view.vue
+++ src/extensions/BlockNode/node-view.vue
@@ -1,31 +1,48 @@
 <template>
-  <node-view-wrapper class="umo-node-view" :data-node-id="props.node.attrs.nodeId">
+  <node-view-wrapper
+    class="umo-node-view"
+    :data-node-id="props.node.attrs.nodeId"
+  >
     <div class="umo-node-paragraph-action" v-show="showToolbar">
-      <div class="umo-node-paragraph-action-item" v-for="item in btns" :key="item.value" @mousedown.prevent
-        @click="onClickBtn(item.value)">{{ item.label }}</div>
+      <div
+        class="umo-node-paragraph-action-item"
+        v-for="item in btns"
+        :key="item.value"
+        @mousedown.prevent
+        @click="onClickBtn(item.value)"
+      >
+        {{ item.label }}
+      </div>
     </div>
-    <node-view-content></node-view-content>
+    <div class="umo-node-paragraph-content">
+      <node-view-content></node-view-content>
+    </div>
   </node-view-wrapper>
 </template>
 
 <script setup lang="ts">
 import { nodeViewProps, NodeViewWrapper, NodeViewContent } from '@tiptap/vue-3'
 import { computed, onMounted, onUnmounted } from 'vue'
-import { MessageEnum, MessageSourceEnum, sendMessageToMain, SendMessageToMainParamsType } from '@/qiankun'
+import {
+  MessageEnum,
+  MessageSourceEnum,
+  sendMessageToMain,
+  SendMessageToMainParamsType,
+} from '@/qiankun'
 
 const btns = [
   {
     label: '替换模板',
-    value: MessageEnum.ReplaceTemplate
+    value: MessageEnum.ReplaceTemplate,
   },
   {
     label: '替换素材',
-    value: MessageEnum.ReplaceMaterial
+    value: MessageEnum.ReplaceMaterial,
   },
   {
     label: '重新生成',
-    value: MessageEnum.Regenerate
-  }
+    value: MessageEnum.Regenerate,
+  },
 ]
 
 const props = defineProps(nodeViewProps)
@@ -39,9 +56,7 @@ const showToolbar = computed(() => {
 
   const isTopLevel = $pos.parent.type.name === 'doc'
 
-  const inThisNode =
-    from >= pos &&
-    from <= pos + props.node.nodeSize
+  const inThisNode = from >= pos && from <= pos + props.node.nodeSize
 
   return isFocused && isTopLevel && inThisNode
 })
@@ -54,8 +69,8 @@ const onClickBtn = (msgType: MessageEnum) => {
   sendMessageToMain({
     msgType,
     msgContent: {
-      nodeId: props.node.attrs.nodeId
-    }
+      nodeId: props.node.attrs.nodeId,
+    },
   })
 }
 
@@ -74,7 +89,7 @@ const replaceBlock = (content: any) => {
         from: pos + 1,
         to: pos + node.nodeSize - 1,
       },
-      content
+      content,
     )
     .run()
 }
@@ -86,21 +101,20 @@ const onReceiveMainAppMessage = (e: any) => {
   if (e.detail.source === MessageSourceEnum.MainApp) {
     const { msgType, msgContent } = e.detail
       .content as SendMessageToMainParamsType
-      if(msgContent.nodeId && msgContent.nodeId === props.node.attrs.nodeId) {
-        switch (msgType) {
-          case MessageEnum.ReplaceMaterial:
-            console.log('替换素材', msgContent)
-            replaceBlock(msgContent.content || '')
-            break
-          case MessageEnum.ReplaceTemplate:
-            console.log('替换模板', msgContent)
-            replaceBlock(msgContent.content || '')
-            break
-          default:
-    
-            break
-        }
+    if (msgContent.nodeId && msgContent.nodeId === props.node.attrs.nodeId) {
+      switch (msgType) {
+        case MessageEnum.ReplaceMaterial:
+          console.log('替换素材', msgContent)
+          replaceBlock(msgContent.content || '')
+          break
+        case MessageEnum.ReplaceTemplate:
+          console.log('替换模板', msgContent)
+          replaceBlock(msgContent.content || '')
+          break
+        default:
+          break
       }
+    }
   }
 }
 
@@ -115,29 +129,33 @@ onUnmounted(() => {
 </script>
 
 <style lang="less">
+.umo-node-paragraph-content {
+  width: 100%;
+}
+
 .umo-node-paragraph-action {
-    max-width: 100%;
-    position: absolute;
-    top: -40px;
-    left: 0;
-    display: flex;
-    align-items: center;
-    column-gap: 10px;
-    background-color: #fff;
-    padding: 4px 16px;
-    box-shadow:
-      0 3px 14px 2px rgba(0, 0, 0, 0.03),
-      0 8px 10px 1px rgba(0, 0, 0, 0.04),
-      0 5px 5px -3px rgba(0, 0, 0, 0.08);
-    border-radius: 4px;
-    border: 1px solid rgba(0, 0, 0, 0.08);
+  max-width: 100%;
+  position: absolute;
+  top: -40px;
+  left: 0;
+  display: flex;
+  align-items: center;
+  column-gap: 10px;
+  background-color: #fff;
+  padding: 4px 16px;
+  box-shadow:
+    0 3px 14px 2px rgba(0, 0, 0, 0.03),
+    0 8px 10px 1px rgba(0, 0, 0, 0.04),
+    0 5px 5px -3px rgba(0, 0, 0, 0.08);
+  border-radius: 4px;
+  border: 1px solid rgba(0, 0, 0, 0.08);
 
-    .umo-node-paragraph-action-item {
-      cursor: pointer;
-    }
-
-    .umo-node-paragraph-action-item:hover {
-      color: aqua;
-    }
+  .umo-node-paragraph-action-item {
+    cursor: pointer;
   }
+
+  .umo-node-paragraph-action-item:hover {
+    color: aqua;
+  }
+}
 </style>
